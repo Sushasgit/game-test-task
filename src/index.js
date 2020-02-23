de54f073b96e60bb7071b902fa59f3bcd5d0ee1c
Susha <Susha@Andreys-MacBook-6.local>
2020-02-23 20:15:34 +0200

    Added basic project setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div className="container">
+        <div className="column">
+            Test
+        </div>
+    </div>
+);
+
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
