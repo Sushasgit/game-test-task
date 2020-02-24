5bc98e86f8705f035975391f6262752b36f73595
Susha <Susha@Andreys-MacBook-6.local>
2020-02-24 17:37:11 +0200

    Fixes autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -32,9 +32,7 @@ module.exports = {
             loader: 'postcss-loader',
             options: {
                 plugins: [
-                    autoprefixer({
-                        browsers:['ie >= 8', 'last 4 version']
-                    })
+                    autoprefixer()
                 ],
                 sourceMap: true
             }
